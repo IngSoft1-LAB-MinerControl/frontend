e6d3a42e2416e08e7b0586a12a5b0a0e8221adb9
SantinoQuevedo <santino.quevedo@mi.unc.edu.ar>
2025-09-15 19:58:50 -0300

    SCRUM-8 mejorando diseño del inicio

--- src/containers/WelcomePage/HomePage.tsx
+++ src/containers/WelcomePage/HomePage.tsx
@@ -1,10 +1,33 @@
 import "./HomePage.css";
-import WelcomeForm from "./WelcomeForm";
+import InputField from "../../components/InputField";
+import Button from "../../components/Button";
 
 export default function HomePage() {
   return (
     <div className="home-page">
-      <WelcomeForm />
+      <form className="form-container">
+        <h1 className="form-title">Bienvenido/a!</h1>
+
+        <div className="form-input">
+          <div className="input-group">
+            <label className="form-label">Nombre</label>
+            <InputField placeholder="Ingrese su nombre" />
+          </div>
+
+          <div className="input-group">
+            <label className="form-label">Fecha de nacimiento</label>
+            <InputField
+              type="date"
+              placeholder="Ingrese su fecha de nacimiento"
+            />
+          </div>
+        </div>
+
+        <div className="form-button">
+          <Button label="Crear Partida" />
+          <Button label="Listar Partidas" />
+        </div>
+      </form>
     </div>
   );
 }
