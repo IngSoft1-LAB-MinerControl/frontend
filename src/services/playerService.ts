0c01ce7c129f558682e8dbdcddcd6863cf666605
SantinoQuevedo <santino.quevedo@mi.unc.edu.ar>
2025-09-23 15:18:47 -0300

    SCRUM-11 implementado servicio para crear el jugador

--- src/services/playerService.ts
+++ src/services/playerService.ts
@@ -1,25 +1,26 @@
 import { httpServerUrl } from "./config";
 
-interface Player {
+export interface Player {
   name: string;
-  datebirth: string;
+  birthdate: string;
   owner: boolean;
 }
 
-async function createPlayer(player: Player): Promise<string> {
+export interface PlayerResponse {
+  id: number;
+}
+
+async function createPlayer(player: Player): Promise<number> {
   const response = await fetch(`${httpServerUrl}/players`, {
     method: "POST",
     headers: {
-      "Content-Type": "application/json",
+      "Content-Type": "application/json", // el cuerpo de la petición (el Player) está en formato JSON
     },
-    body: JSON.stringify(player), // se convierte al player en un JSON
+    body: JSON.stringify(player), // como en content-type dijimos que era applicacition/json, el cuerpo de la peticion POST debe ser un json y no JS. Entonces convertimos el objeto JS a un JSON
   });
-  if (!response.ok) {
-    throw new Error("Error al crear jugador");
-  }
 
-  const data = (await response.json()) as { player_id: string }; // parseamos JSON
-  return data.player_id; // devolvemos un string
+  const data: PlayerResponse = await response.json(); // parseamos la respuesta del backend {"id: _"} a un json
+  return data.id; // devolvemos un id
 }
 
 const playerService = {
