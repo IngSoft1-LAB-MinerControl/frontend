33e3d0155a1504c867ce1c731084f8061c305560
cotib2 <cbuteler@mi.unc.edu.ar>
2025-10-14 10:47:01 -0300

    mini merge fixes

--- src/components/DraftPile.tsx
+++ src/components/DraftPile.tsx
@@ -1,6 +1,7 @@
 import "./DraftPile.css";
-import CardBase from "./Cards/CardBase";
 import type { CardResponse } from "../services/cardService";
+import Detective from "./Cards/Detectives";
+import Event from "./Cards/Events";
 
 interface DraftPileProps {
   cards: CardResponse[];
@@ -12,14 +13,27 @@ export default function DraftPile({ cards }: DraftPileProps) {
       <h3 className="draft-pile-title">Draft</h3>
       <div className="draft-pile">
         {cards.length > 0
-          ? cards.map((card) => (
-              <CardBase
-                key={card.card_id}
-                card_id={card.card_id}
-                shown={true}
-                size="medium"
-              />
-            ))
+          ? cards.map((card) =>
+              card.type === "detective" ? (
+                <Detective
+                  key={card.card_id}
+                  card_id={card.card_id}
+                  shown={true}
+                  size="mini"
+                  name={card.name}
+                  // quantity_set={lastDiscarded.quantity_set}
+                />
+              ) : (
+                <Event
+                  key={card.card_id}
+                  card_id={card.card_id}
+                  shown={true}
+                  size="mini"
+                  name={card.name}
+                  // quantity_set={lastDiscarded.quantity_set}
+                />
+              )
+            )
           : Array.from({ length: 3 }).map((_, i) => (
               <div key={i} className="draft-placeholder" />
             ))}
