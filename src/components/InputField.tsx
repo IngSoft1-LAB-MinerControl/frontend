1c68c7249f9b0c2173b81afc57c5a6101f04f237
SantinoQuevedo <santino.quevedo@mi.unc.edu.ar>
2025-10-15 16:15:13 -0300

    SCRUM-79 implementados diseños para el formulario de inicio

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -8,6 +8,7 @@ type Props = {
   value: string;
   maxLength?: number;
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  error: boolean;
 };
 
 export default function InputField({
@@ -17,11 +18,12 @@ export default function InputField({
   value,
   maxLength,
   onChange,
+  error = false,
 }: Props) {
   return (
     <input
       id={id}
-      className="input-field"
+      className={`input-field ${error ? "input-error" : ""}`}
       type={type}
       placeholder={placeholder}
       value={value}
